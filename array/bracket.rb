b4f89fae646f2e9876e5ed977445bf8698c166a1
hihumi <yui.maa5800@gmail.com>
2016-12-19 21:17:59 +0900

    make dir and file

---
+++ array/bracket.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+
+puts Array.[](1, 2, 3)
+puts
+
+a = Array.[](1.1, 2.2, 3.3)
+puts a
+puts
+p a
+puts
+
+b = Array.[](1, 2.2, 'a', "xyz")
+puts b
+puts
+p b
+puts
+
+puts [1, 2.2, 'a', "xyz"]
+puts
+p [1, 2.2, 'a', "xyz"]
