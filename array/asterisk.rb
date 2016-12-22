1fb7e5ec03931e3c70d9fd63fbce6dd67607f82c
hihumi <yui.maa5800@gmail.com>
2016-12-23 07:58:59 +0900

    add file

---
+++ array/asterisk.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+
+a = [1, 2, 3] * 5
+puts a
+p a
+puts
+
+b = [1.1, 2.2, 3.3] * 3
+puts b
+p b
+puts
+
+c = ['a', 'b', "cdefg"] * 2
+puts c
+p c
+puts
+
+d = ['R', 'u', 'b', 'y'] * "!!!" + "!!!"
+puts d
+p d
