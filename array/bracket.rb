5da7db836b2d852e2d1aa4f39a2d734c65854160
hihumi <yui.maa5800@gmail.com>
2016-12-24 11:21:37 +0900

    add file

---
+++ array/bracket.rb
@@ -0,0 +1,35 @@
+#!/usr/bin/env ruby
+
+a = ['a', 'b', "cdefg"]
+puts a
+p a
+puts
+
+puts a[0] + a[1]
+p a[0] + a[1]
+puts
+
+puts a[3]
+p a[3]
+puts
+
+puts a[0, 2]
+p a[0, 2]
+puts 
+
+puts a[0..2]
+p a[0..2]
+puts
+
+puts a[0...2]
+p a[0...2]
+puts
+
+puts a[-3, 3]
+p a[-3, 3]
+puts
+
+puts a[10..20]
+p a[10..20]
+
+
