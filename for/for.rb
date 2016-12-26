20ddf068f502361d70eb8f3212a88574dfc42df9
hihumi <yui.maa5800@gmail.com>
2016-12-26 09:57:06 +0900

    make dir and file

---
+++ for/for.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+
+for x in [1, 2, 3, 4, 5]
+  puts x
+end
+puts
+
+y = [1, 2, 3, 4, 5]
+for i in y
+    puts i
+end
+puts
+
+# each
+[1, 2, 3, 4, 5].each do |z|
+  puts z
+end
+puts
+
+y.each do |j|
+  puts j
+end
